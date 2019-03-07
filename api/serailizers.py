d880210b2802d744e987dbdbe33b2a2c867356b9
Vikas Passi <vikasvisking007@gmail.com>
2019-03-07 06:59:44 +0530

    add serializers

---
+++ api/serailizers.py
@@ -0,0 +1,30 @@
+from api.models import Match, Sports, Market, Selection
+from rest_framework import serializers
+
+class SportSerializer(serializers.ModelSerializer):
+	class Meta:
+		model =Sports
+		fields = ('id', 'name')
+
+class SelectionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Selection
+		fields = ('id', 'name', 'odds')
+
+class MarketSerializer(serializers.ModelSerializer):
+	Selection = SelectionSerializer()
+	class Meta:
+		model = Market
+		fields = ('id', 'name', 'selections')
+
+class MatchListSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Match
+		fields = ('id', 'url', 'name', 'startTime')
+
+class MatchDetailSerializer(serializers.ModelSerializer):
+	sport = SportSerializer()
+	market = MarketSerializer()
+	class Meta:
+		model = Match
+		fields = ('id', 'url', 'name', 'startTime', 'sport', 'market' )
